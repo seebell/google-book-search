dfbf1cb2ca00a90f623f080453e08b187d77196c
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-08-21 02:41:02 -0400

    set up the api key and API

---
+++ client/src/utils/API.js
@@ -0,0 +1,22 @@
+
+const dotenv = require("dotenv");
+dotenv.config();
+const axios = require("axios");
+
+export default {
+    searchbyKeywords: function(keywords) {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${keywords}&key=${process.env.REACT_APP_BOOKS_KEY}`);
+    },
+    searchbyKeywordsAndAuthor: function(keywords, author) {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${keywords}+inauthor:${author}&key=${process.env.REACT_APP_BOOKS_KEY}`);
+    },
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+    addBook: function(data) {
+        return axios.post("/api/books", data);
+    },
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
+    }
+}
