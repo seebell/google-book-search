1348d3dd49e6cd4f51a6b88ec2c60a5ea5e65aab
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-08-22 07:46:11 -0400

    heroku

--- server.js
+++ server.js
@@ -6,20 +6,22 @@ const routes= require ("./routes")
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
 app.use(routes)
 
-//Connet to mongoose
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-book-search", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-book-search", 
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false});
 
 
 app.listen(PORT, () => {
